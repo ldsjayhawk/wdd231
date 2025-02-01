7cd91b7c25214f5406e3c10d6794077b51fb5286
ldsjayhawk <crisbecca@gmail.com>
2025-02-01 17:53:43 -0600

    fixes to directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,86 +1,63 @@
 import { copyrightYear, lastModifiedDate } from "./footer.mjs";
 
-// responsive menu
-const hamburger = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamburger.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamburger.classList.toggle('open');
-});
-
-// variables for grid - list toggle
+const jsonfile = 'data/members.json'
+const cards = document.querySelector('#cards')
 const grid = document.querySelector("#grid");
 const list = document.querySelector("#list");
-const jsonFile = 'data/members.json';
-let data = [];
 
-const membercard = document.querySelector("membercard");
+async function getMemberData() {
+    const response = await fetch(jsonfile)
+    const data = await response.json();
+    console.log(data.members)
+    displayMembers(data.members);
+}
 
-// get data from the json file
-export async function apiFetch(jsonfile) {
-    try {
-        const response = await fetch(jsonfile);
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data)
-            return data
-        } else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-            console.log("Error retreiving data", error);
-            }
+const displayMembers = (members) => {
+        members.forEach((member) => {
+            let card = document.createElement("div")
+            let companyname = document.createElement('h3')
+            let logo = document.createElement('img')
+            let address = document.createElement('p')
+            let phone = document.createElement('p')
+            let url = document.createElement('a')
+
+            // divElement.classList.add("member")
+            // `<div class="member">
+            companyname.textContent = `${member.name}`
+            logo.setAttribute('src', member.image);
+            logo.setAttribute('alt', `company logo for ${member.name}`);
+            logo.setAttribute('loading', 'lazy');
+            logo.setAttribute('width', '150');
+            logo.setAttribute('height', '150');
+            url.setAttribute('href',member.url)
+            url.textContent = member.url;
+            address.textContent = `${member.address}`
+            phone.textContent = `${member.phone}`
+            
+            card.appendChild(companyname); 
+            card.appendChild(logo);
+            card.appendChild(address);
+            card.appendChild(phone);
+            card.appendChild(url);
+            cards.appendChild(card);
+            card.classList.add('member')
+        }); 
     }
 
-function displayGrid(data) {
-    data.forEach(member =>  {
-        membercard.innerHTML = "";
-        const divElement = document.createElement("div")
-        divElement.classList.add("member")
-        // `<div class="member">
-        `<h3>${member.company}</h3>
-        <img src="${member.image}" alt="company logo" width="100" height="auto"> 
-        <h4>Address: ${member.address}</h4>
-        <h4>Phone: ${member.phone}</h4>
-        <h4>${member.url}</h4>
-        </div>`;
-        membercard.appendChild(divElement);
-    });
-}
-
-function displayList(data) {
-    const table = document.createElement("table")
-    data.forEach(member =>  {
-        const row = document.createElement("tr")
-        row.innerHTML =
-        `<div class="member">
-        <table>
-            <tr>
-                <td><h4>${member.company}</h4></td><td><p>Address: ${member.address}</p></td><td><p>Phone: ${member.phone}</p></td><td><p>${member.url}</p></td>
-            </tr>
-        </table>
-        </div>`
-        table.appendChild(row);
-        },
-    );
-    membercard.appendChild(table);
-}
-
-// when clicking the button, go to the correct function
-grid.addEventListener("click", () => {
-	membercard.classList.add("grid");
-	membercard.classList.remove("list");
-    displayGrid(data);
-});
-
-list.addEventListener("click", () => {
-	membercard.classList.add("list");
-	membercard.classList.remove("grid");
-    displayList(data);
-});
+    // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
     
+    grid.addEventListener("click", () => {
+        cards.classList.add("grid");
+        cards.classList.remove("list");
+    });
+    
+    list.addEventListener("click", () => {
+        cards.classList.add("list");
+        cards.classList.remove("grid");
+    });
+    
+getMemberData();
 
-// footer info ---------------
+// footer info
 copyrightYear();
 lastModifiedDate();
